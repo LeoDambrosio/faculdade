5d619863356a5497e2341bb41b6801b3b99cd414
LEONARDO VIEIRA DAMBROSIO <leonardovieira1409@gmail.com>
2025-03-17 19:16:27 -0300

    realizado em casa

--- EstruturadeDados.c/DiagonalMatriz.c
+++ EstruturadeDados.c/DiagonalMatriz.c
@@ -1,46 +1,28 @@
+//Diagonal
+
 #include <stdio.h>
 
 int main() {
-    // Definindo as matrizes 4x4
-    int matriz_A[4][4];
-    int matriz_B[4][4];
-    int matriz_resultado[4][4] = {0}; // Matriz para armazenar o resultado
+    int matriz [6][6];
+    int i, j;
 
-    // Solicita ao usuário os valores da matriz A
-    printf("Digite os valores da matriz A (4x4):\n");
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            printf("Matriz A[%d][%d]: ", i, j);
-            scanf("%d", &matriz_A[i][j]);
+for(i = 0; i < 5; i++) {
+    for(j = 0; j < 5; j++) {
+        if (i==j)
+        {
+            matriz [i][j] = 1; 
+        }else{
+            matriz [i] [j] = 0;
         }
+        
     }
+}
 
-    // Solicita ao usuário os valores da matriz B
-    printf("\nDigite os valores da matriz B (4x4):\n");
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            printf("Matriz B[%d][%d]: ", i, j);
-            scanf("%d", &matriz_B[i][j]);
-        }
-    }
+for(i = 0; i < 5; i++) {
+    for(j = 0; j < 5; j++) {
+        printf("%d ", matriz [i][j] );
+}
+    printf("\n");
 
-    // Multiplicando as matrizes
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            for (int k = 0; k < 4; k++) {
-                matriz_resultado[i][j] += matriz_A[i][k] * matriz_B[k][j];
-            }
-        }
-    }
-
-    // Exibindo o resultado
-    printf("\nMatriz Resultante (A x B):\n");
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            printf("%d\t", matriz_resultado[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+}
 }
--- EstruturadeDados.c/MultiplicacaoMatriz.c
+++ EstruturadeDados.c/MultiplicacaoMatriz.c
@@ -1,65 +1,41 @@
+
 #include <stdio.h>
-#include <stdlib.h>
-
-// Função para multiplicação de matrizes
-void multiplicaMatrizes(int **A, int **B, int **C, int linA, int colA, int colB) {
-    for (int i = 0; i < linA; i++)
-        for (int j = 0; j < colB; j++) {
-            C[i][j] = 0;
-            for (int k = 0; k < colA; k++)
-                C[i][j] += A[i][k] * B[k][j];
-        }
-}
-
-// Função para exibir a matriz
-void exibeMatriz(int **M, int linhas, int colunas) {
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++)
-            printf("%4d ", M[i][j]);
-        printf("\n");
-    }
-}
 
 int main() {
-    int linA, colA, linB, colB;
-    
-    printf("Digite as dimensoes da matriz A (linhas colunas): ");
-    scanf("%d %d", &linA, &colA);
-    printf("Digite as dimensoes da matriz B (linhas colunas): ");
-    scanf("%d %d", &linB, &colB);
-    
-    if (colA != linB) {
-        printf("Erro: O numero de colunas de A deve ser igual ao numero de linhas de B.\n");
-        return 1;
+   
+    int matriz_A[6][6];
+    int matriz_B[6][6];
+    int matriz_resultado[6][6] = {0};
+   
+    printf("Digite os valores da matriz A (6x6):\n");
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            printf("Matriz A[%d][%d]: ", i, j);
+            scanf("%d", &matriz_A[i][j]);
+        }
     }
-    
-    int **A = (int **)malloc(linA * sizeof(int *));
-    int **B = (int **)malloc(linB * sizeof(int *));
-    int **C = (int **)malloc(linA * sizeof(int *));
-    
-    for (int i = 0; i < linA; i++) A[i] = (int *)malloc(colA * sizeof(int));
-    for (int i = 0; i < linB; i++) B[i] = (int *)malloc(colB * sizeof(int));
-    for (int i = 0; i < linA; i++) C[i] = (int *)malloc(colB * sizeof(int));
-    
-    printf("Digite os elementos da matriz A:\n");
-    for (int i = 0; i < linA; i++)
-        for (int j = 0; j < colA; j++)
-            scanf("%d", &A[i][j]);
-    
-    printf("Digite os elementos da matriz B:\n");
-    for (int i = 0; i < linB; i++)
-        for (int j = 0; j < colB; j++)
-            scanf("%d", &B[i][j]);
-    
-    multiplicaMatrizes(A, B, C, linA, colA, colB);
-    
-    printf("\nResultado da multiplicacao:\n");
-    exibeMatriz(C, linA, colB);
-    
-    for (int i = 0; i < linA; i++) free(A[i]);
-    for (int i = 0; i < linB; i++) free(B[i]);
-    for (int i = 0; i < linA; i++) free(C[i]);
-    free(A); free(B); free(C);
-    
-    return 0;
+
+    printf("\nDigite os valores da matriz B (6x6):\n");
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            printf("Matriz B[%d][%d]: ", i, j);
+            scanf("%d", &matriz_B[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            for (int k = 0; k < 6; k++) {
+                matriz_resultado[i][j] += matriz_A[i][k] * matriz_B[k][j];
+            }
+        }
+    }
+
+    printf("\nMatriz Resultante (A x B):\n");
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            printf("%d\t", matriz_resultado[i][j]);
+        }
+        printf("\n");
+    }
 }
