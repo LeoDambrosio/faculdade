19d3988f154c461055fcda3812c5bec596e437ae
LEONARDO VIEIRA DAMBROSIO <leonardovieira1409@gmail.com>
2025-04-03 20:35:14 -0300

    realizado em sala

--- Poo/lista2/src/br/com/leonardovieira/exercio16/Exercicio16.java
+++ Poo/lista2/src/br/com/leonardovieira/exercio16/Exercicio16.java
@@ -1,5 +1,27 @@
 package lista2.src.br.com.leonardovieira.exercio16;
 
-public class Exercicio16 {
+import java.util.Scanner;
 
+public class Exercicio16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite uma frase: ");
+        String frase = scanner.nextLine();
+        
+        String resultado = substituirVogais(frase);
+        
+        System.out.println("Frase modificada:");
+        System.out.println(resultado);
+        
+        scanner.close();
+    }
+    
+    public static String substituirVogais(String texto) {
+        return texto.toUpperCase()
+                   .replace('A', '4')
+                   .replace('E', '3')
+                   .replace('I', '1')
+                   .replace('O', '0');
+    }
 }
