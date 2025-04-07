fe95806a4c85f97b4ab28a29e9456bbff740c492
LEONARDO VIEIRA DAMBROSIO <leonardovieira1409@gmail.com>
2025-04-07 20:14:57 -0300

    realizado em sala

--- Poo/Lista4/src/br/com/leonardovieira/exercicio11/AtributosInstanciaClasse.java
+++ Poo/Lista4/src/br/com/leonardovieira/exercicio11/AtributosInstanciaClasse.java
@@ -3,3 +3,15 @@ package Lista4.src.br.com.leonardovieira.exercicio11;
 public class AtributosInstanciaClasse {
 
 }
+//Considere a classe abaixo:
+//class Exemplo {
+    //static int contador = 0;
+    //int valor = 10;
+//}
+//O que acontece se criarmos duas instâncias de Exemplo e modificarmos contador?
+//A) Cada instância terá seu próprio contador separado.
+//B) O valor de contador será compartilhado entre todas as instâncias.
+//C) contador não pode ser alterado.
+//D) contador é criado toda vez que uma instância é criada.
+
+//Resposta Correta letra B.
--- Poo/Lista4/src/br/com/leonardovieira/exercicio8/Encapsulamento.java
+++ Poo/Lista4/src/br/com/leonardovieira/exercicio8/Encapsulamento.java
@@ -1,5 +1,12 @@
 package Lista4.src.br.com.leonardovieira.exercicio8;
 
-public class Saida {
+public class Encapsulamento {
 
 }
+//Qual princípio da POO garante que os atributos de uma classe só podem ser acessados por meio de métodos específicos?
+//A) Polimorfismo
+//B) Herança
+//C) Encapsulamento
+//D) Abstração
+
+// Resposta Correta letra C.
