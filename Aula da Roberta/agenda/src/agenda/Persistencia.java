3ad4af8a7b54af843bcd108133f1d86858b903ae
Leonardo635 <leonardovieira1409@gmail.com>
2025-04-16 23:52:56 -0300

    realizado em sala

--- Aula da Roberta/agenda/src/agenda/Persistencia.java
+++ Aula da Roberta/agenda/src/agenda/Persistencia.java
@@ -6,6 +6,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
 public class Persistencia {
 
