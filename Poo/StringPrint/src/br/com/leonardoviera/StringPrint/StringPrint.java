c93100bd2710a370901b3bd9420dd11f5128fb37
LEONARDO VIEIRA DAMBROSIO <leonardovieira1409@gmail.com>
2025-05-22 20:20:45 -0300

    realizado em sala

--- Poo/StringPrint/src/br/com/leonardoviera/StringPrint/StringPrint.java
+++ Poo/StringPrint/src/br/com/leonardoviera/StringPrint/StringPrint.java
@@ -1,3 +1,12 @@
 public class StringPrint {
 
+    private String valor;
+
+    public void add(String valor) {
+        this.valor = valor;
+    }
+
+    public String get() {
+        return this.valor;
+    }
 }
