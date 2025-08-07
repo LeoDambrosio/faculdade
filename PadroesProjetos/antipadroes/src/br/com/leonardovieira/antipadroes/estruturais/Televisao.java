b0698f9a9f8f1c5b73d20e36514410ebef82aeeb
LEONARDO VIEIRA DAMBROSIO <leonardovieira1409@gmail.com>
2025-08-06 21:58:26 -0300

    realiazdo em sala

---
+++ PadroesProjetos/antipadroes/src/br/com/leonardovieira/antipadroes/estruturais/Televisao.java
@@ -0,0 +1,23 @@
+package antipadroes.src.br.com.leonardovieira.antipadroes.estruturais;
+
+public class Televisao implements Dispositivo{
+
+    @Override
+    public void ligar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+    }
+
+    @Override
+    public void desligar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'desligar'");
+    }
+
+    @Override
+    public void aumentarVolume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'aumentarVolume'");
+    }
+
+}
