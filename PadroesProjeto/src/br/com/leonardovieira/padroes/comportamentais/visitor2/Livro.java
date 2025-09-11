480cdf29ebcf64039bffce95af214a80f385947d
LEONARDO VIEIRA DAMBROSIO <leonardovieira1409@gmail.com>
2025-09-10 22:00:19 -0300

    realizado em sala

---
+++ PadroesProjeto/src/br/com/leonardovieira/padroes/comportamentais/visitor2/Livro.java
@@ -0,0 +1,24 @@
+package br.com.leonardovieira.padroes.comportamentais.visitor2;
+public class Livro implements Item {
+
+    private String titulo;
+    private double preco;
+
+    public Livro(String titulo, double preco) {
+        this.titulo = titulo;
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public void aceitar(Visitante visitante) {
+        visitante.visitar(this);
+    }
+}
